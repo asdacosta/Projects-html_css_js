984678657e2d0a8e7b459775fe36fea5f8daa100
asdacosta <asdacosta18@gmail.com>
2023-06-10 19:14:10 -0700

    Add if statements to print strings replacing numbers divisible by particular numbers

--- core-js/fizzbuzz.js
+++ core-js/fizzbuzz.js
@@ -17,5 +17,12 @@
 let num = parseInt(prompt("Enter the number: "));
 
 for (let m = 1; m <= num; m++) {
-    
+    if (m % 3 === 0 && m % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (m % 3 === 0) {
+        console.log('Fizz');
+    } else if (m % 5 === 0) {
+        console.log('Buzz');
+    }
+    console.log(m);
 }
