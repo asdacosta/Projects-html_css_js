5d40c5ee34129a2f6b757de3ffb42f8515eaa1d8
asdacosta <asdacosta18@gmail.com>
2023-06-21 09:33:55 -0700

    Transfer algorithm to separate file

--- core-js/05_fizzbuzz/fizzbuzz.js
+++ core-js/05_fizzbuzz/fizzbuzz.js
@@ -1,19 +1,3 @@
-// Algorithm
-// 1. Take a number input from user.
-// 2. Print numbers from 1 to user number in the console.
-// 3. In the printed numbers:
-//      i. If a number is divisible by 3 and 5, replace it with FizzBuzz
-//      ii. If a number is divisible by 3, replace it with Fizz
-//      iii. If a number is divisible by 5, replace it with Buzz
-
-// Pseudocode
-// 1. Use parseInt and prompt fxns to take number input.
-// 2. Use for loop to print numbers in console.
-// 3. All results of the if statements should be in console:
-//      i. Use if, modulus and && operator.
-//      ii. Use if and modulus.
-//      iii. Use if and modulus.
-
 let num = parseInt(prompt("Enter the number: "));
 
 for (let m = 1; m <= num; m++) {
