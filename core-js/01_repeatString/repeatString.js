84dc972b4d02345c29137b8a93531870ce23f4dd
asdacosta <asdacosta18@gmail.com>
2023-07-10 12:41:52 -0700

    Return Error when the number parsed to the function is negative

--- core-js/01_repeatString/repeatString.js
+++ core-js/01_repeatString/repeatString.js
@@ -1,10 +1,14 @@
 const repeatString = function(string, num) {
     let str = '';
     
+    if (num < 0) {
+        return 'ERROR';
+    }
     for (m = 0; m < num; m++) {
         str += string;
     }
     return str;
+
 };
 
 module.exports = repeatString;
