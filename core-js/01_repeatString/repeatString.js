14d4eb4d5fe4b0d46e9e26591b2bde1489e11b7b
asdacosta <asdacosta18@gmail.com>
2023-07-10 12:35:53 -0700

    Repeat a given string a number of times

--- core-js/01_repeatString/repeatString.js
+++ core-js/01_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let str = '';
+    
+    for (m = 0; m < num; m++) {
+        str += string;
+    }
+    return str;
 };
 
 module.exports = repeatString;
