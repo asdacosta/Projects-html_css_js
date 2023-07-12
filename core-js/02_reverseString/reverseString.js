61ba79eb6f5d2316f3bec978400def401a82f42d
asdacosta <asdacosta18@gmail.com>
2023-07-12 08:08:43 -0700

    Reverse string

--- core-js/02_reverseString/reverseString.js
+++ core-js/02_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseStr = '';
+    for (m = str.length - 1; m >= 0; m--) {
+        reverseStr += str[m]
+    }
+    return reverseStr;
 };
 
 module.exports = reverseString;
