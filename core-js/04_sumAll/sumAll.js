dfd81595af5a803db715c6dc3e128f41b7601f8f
asdacosta <asdacosta18@gmail.com>
2023-07-14 13:05:37 -0700

    Return error when arg is negative or not a number

--- core-js/04_sumAll/sumAll.js
+++ core-js/04_sumAll/sumAll.js
@@ -1,5 +1,8 @@
 const sumAll = function(int1, int2) {
     let sum = 0;
+    if (int1 < 0 || int2 < 0 || typeof int1 != 'number' || typeof int2 != 'number') {
+        return 'ERROR';
+    }
     for (m = Math.min(int1, int2); m <= Math.max(int1, int2); m++) {
         sum += m;
     }
