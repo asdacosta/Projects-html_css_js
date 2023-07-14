3697c6871eec878a2e43b8377092ef48eab2be21
asdacosta <asdacosta18@gmail.com>
2023-07-14 09:57:29 -0700

    Add a second approach

--- core-js/03_removeFromArray/removeFromArray.js
+++ core-js/03_removeFromArray/removeFromArray.js
@@ -10,6 +10,25 @@ const removeFromArray = function(arrs, ...args) {
     return arrs;
 };
 
+// Second method
+// const removeFromArray = function(array, ...elements) {
+//     const newArray = [];
+
+//     for (let m = 0; m < array.length; m++) {
+//         let found = false;
+//         for (let i = 0; i < elements.length; i++) {
+//             if (elements[i] === array[m]) {
+//                 found = true;
+//                 continue;
+//             }
+//         }
+//         if (!found) {
+//             newArray.push(array[m]);
+//         }
+//     }
+//     return newArray;
+// };
+
 // Simpler method
 // Returns new array containing elements from arrs array that pass callback fxn
 // Callback fxn takes each element/arr of arrs and checks if it's included in args array
@@ -19,5 +38,7 @@ const removeFromArray = function(arrs, ...args) {
 // const removeFromArray = function(arrs, ...args) {
 //     return arrs.filter(arr => !args.includes(arr));
 // }
+
+
 
 module.exports = removeFromArray;
