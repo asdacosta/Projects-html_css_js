ca8b0a0964e8bc93b169f5ce7982c1bf8977a65c
asdacosta <asdacosta18@gmail.com>
2023-07-15 14:04:41 -0700

    Sum all numbers between and inclusive of integers

--- core-js/04_sumAll/sumAll.js
+++ core-js/04_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let sum = 0;
+    for (m = Math.min(int1, int2); m <= Math.max(int1, int2); m++) {
+        sum += m;
+    }
+    return sum;
 };
 
 module.exports = sumAll;
