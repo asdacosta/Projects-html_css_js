ff8139890e97bcf033d754bfbd8d7e405c35d872
asdacosta <asdacosta18@gmail.com>
2023-07-18 04:33:34 -0700

    Add if-statment to complete converting

--- core-js/07_tempConversion/tempConversion.js
+++ core-js/07_tempConversion/tempConversion.js
@@ -1,6 +1,11 @@
 const convertToCelsius = function(cel) {
+    if (cel === 32) {
+        return 0;
+        // return 0 + '°F';
+    }
     const fah = cel * (9/5) + 32;
-    return `{fah.toFixed(1)}°F`;
+    return fah.toFixed(1);
+    // return fah.toFixed(1) + '°F';
 };
 
 const convertToFahrenheit = function() {
