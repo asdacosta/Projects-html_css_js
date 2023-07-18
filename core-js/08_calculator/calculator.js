10bc5557a45886123eb15ad8614da9d6068ff68e
asdacosta <asdacosta18@gmail.com>
2023-07-18 05:17:17 -0700

    Create add and subtract functions

---
+++ core-js/08_calculator/calculator.js
@@ -0,0 +1,33 @@
+const add = function(m, n) {
+	return m + n;
+};
+
+const subtract = function(m, n) {
+	return m - n;
+};
+
+const sum = function() {
+	
+};
+
+const multiply = function() {
+
+};
+
+const power = function() {
+	
+};
+
+const factorial = function() {
+	
+};
+
+
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
