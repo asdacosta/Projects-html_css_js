9d97e2859bfc7dbb379441921dc803f5df82dd1b
asdacosta <asdacosta18@gmail.com>
2023-07-18 11:44:23 -0700

    Ignore punctuations and white space when checking whether string is palindrome

--- core-js/09_palindromes/palindromes.js
+++ core-js/09_palindromes/palindromes.js
@@ -1,15 +1,23 @@
 const palindromes = function (string) {
     // return string.split('').reverse().join('') === string;
 
-    if (string[string.length - 1] === '!') {
-        string = string.slice(0, string.length - 1);
+    // if (string[string.length - 1] === '!') {
+    //     string = string.slice(0, string.length - 1);
+    // }
+
+    let str = '';
+    for (n = 0; n <= string.length - 1; n++) {
+        if (string[n] === '!' || string[n] === ',' || string[n] === '.' || string[n] === ' ') {
+            continue;
+        }
+        str += string[n];
     }
 
     let strReverse = '';
-    for (m = string.length - 1; m >= 0; m--) {
-        strReverse += string[m];
+    for (m = str.length - 1; m >= 0; m--) {
+        strReverse += str[m];
     }
-    return strReverse.toLowerCase() === string.toLowerCase();
+    return strReverse.toLowerCase() === str.toLowerCase();
 };
 
 module.exports = palindromes;
