abfd59d1c03d0504d59b52cb68f4249f9213279f
asdacosta <asdacosta18@gmail.com>
2023-07-19 09:18:37 -0700

    Check if a word is a palindrome

--- core-js/09_palindromes/palindromes.js
+++ core-js/09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
 const palindromes = function (string) {
-    return string.split('').reverse().join('') === string;
+    // return string.split('').reverse().join('') === string;
+
+    let strReverse = '';
+    for (m = string.length - 1; m >= 0; m--) {
+        strReverse += string[m];
+    }
+    return strReverse === string;
 };
 
 module.exports = palindromes;
