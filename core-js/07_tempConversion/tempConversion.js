4d4c0a07241b40493a4cbc7fca680df57655e452
asdacosta <asdacosta18@gmail.com>
2023-07-19 05:49:34 -0700

    Remove repetition

--- core-js/07_tempConversion/tempConversion.js
+++ core-js/07_tempConversion/tempConversion.js
@@ -1,8 +1,4 @@
 const convertToCelsius = function(fah) {
-    if (fah === 32) {
-        return 0;
-        // return 0 + '°F';
-    }
     const cel = (fah-32) * 5/9;
     celValue = cel.toFixed(1);
     return parseFloat(celValue);
