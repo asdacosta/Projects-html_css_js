3f2e394d5e93c0c8bb1eabf37e49bcbebd5763d0
asdacosta <asdacosta18@gmail.com>
2023-07-19 09:44:36 -0700

    Ignore exclamations appended to the word

--- core-js/09_palindromes/palindromes.js
+++ core-js/09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
 const palindromes = function (string) {
     // return string.split('').reverse().join('') === string;
 
+    if (string[string.length - 1] === '!') {
+        string = string.slice(0, string.length - 1);
+    }
+
     let strReverse = '';
     for (m = string.length - 1; m >= 0; m--) {
         strReverse += string[m];
