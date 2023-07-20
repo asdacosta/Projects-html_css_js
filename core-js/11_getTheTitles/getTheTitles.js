47ea523b0573f5b9ff14026db811fede0633227f
asdacosta <asdacosta18@gmail.com>
2023-07-20 02:09:47 -0700

    Select value of titles in an array of objects

--- core-js/11_getTheTitles/getTheTitles.js
+++ core-js/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let values1 = Object.values(array[0]);
+    let values2 = Object.values(array[1]);
+    let title = [values1[0], values2[0]];
+    return title;
 };
 
 module.exports = getTheTitles;
