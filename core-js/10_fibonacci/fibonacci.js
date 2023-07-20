a8cc329981c1569f5d70d160345e71690009a7d5
asdacosta <asdacosta18@gmail.com>
2023-07-19 17:11:17 -0700

    Find the nth number in Fibonacci seq

--- core-js/10_fibonacci/fibonacci.js
+++ core-js/10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    let fib = [1, 1],
+        prevSum = 0;
+    for (m = 0; m < nth - 2; m++) {
+        prevSum = fib[m] + fib[m+1];
+        fib.push(prevSum);
+    }
+    return fib[nth - 1];
 };
 
 module.exports = fibonacci;
