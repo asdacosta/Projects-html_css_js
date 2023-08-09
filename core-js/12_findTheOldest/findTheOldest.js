22542062b70e8d573f1ead3e50fccaa3dd2fa86e
asdacosta <asdacosta18@gmail.com>
2023-08-09 05:39:33 -0700

    Obtain oldest age

--- core-js/12_findTheOldest/findTheOldest.js
+++ core-js/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldAge = arr.reduce((age, obj) => obj.yearOfDeath - obj.yearOfBirth > age, 0);
 };
 
 module.exports = findTheOldest;
