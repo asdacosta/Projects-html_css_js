4e4c97c13c49710c83f7b794a571d2a0132f9fba
asdacosta <asdacosta18@gmail.com>
2023-08-09 09:55:05 -0700

    Find the max age amongst objects whose death year is not specified

--- core-js/12_findTheOldest/findTheOldest.js
+++ core-js/12_findTheOldest/findTheOldest.js
@@ -1,10 +1,13 @@
 const findTheOldest = function(arr) {
     let oldAge = arr.reduce((age, obj) => {
+        if (obj.yearOfDeath === undefined) {
+            obj.yearOfDeath = 2023;
+        }
         const ageDiff = obj.yearOfDeath - obj.yearOfBirth;
         return ageDiff > age ? ageDiff : age;
         }, 0);
 
-    for (m = 0; m < arr.length; m++) {
+    for (let m = 0; m < arr.length; m++) {
         if (oldAge === arr[m].yearOfDeath - arr[m].yearOfBirth) {
             return arr[m];
         }
