056bbf6cca9195fc3098622d1accb07e8e073a6c
asdacosta <asdacosta18@gmail.com>
2023-12-19 16:08:43 +0000

    Create merge sort logic without sort() method

--- core-js/13_mergesort/mergesort.js
+++ core-js/13_mergesort/mergesort.js
@@ -1,25 +1,46 @@
 #!/usr/bin/node
 function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
   const middle = Math.floor(array.length / 2);
   const leftHalf = array.slice(0, middle);
   const rightHalf = array.slice(middle);
 
-  if (leftHalf.length <= 2 && rightHalf.length <= 2) {
-    if (leftHalf.length === 2) {
-      leftHalf.sort((a, b) => a - b);
-    }
-    if (rightHalf === 2) {
-      rightHalf.sort((a, b) => a - b);
-    }
-    let newArray = [...leftHalf, ...rightHalf];
-    newArray.sort((a, b) => a - b);
-    return newArray;
-  } else {
-    const realLeft = mergeSort(leftHalf);
-    const realRight = mergeSort(rightHalf);
-    let newArray = [...realLeft, ...realRight];
-    newArray.sort((a, b) => a - b);
-    return newArray;
-  }
+  const sortedLeft = mergeSort(leftHalf);
+  const sortedRight = mergeSort(rightHalf);
+
+  return merge(sortedLeft, sortedRight);
 }
+
+function merge(left, right) {
+  const mergedArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      mergedArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      mergedArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  // Add remaining elements
+  while (leftIndex < left.length) {
+    mergedArray.push(left[leftIndex]);
+    leftIndex++;
+  }
+  while (rightIndex < right.length) {
+    mergedArray.push(right[rightIndex]);
+    rightIndex++;
+  }
+
+  return mergedArray;
+}
+console.log(mergeSort([4, 3, 2]));
+console.log(mergeSort([1, 6, 1, 8]));
 console.log(mergeSort([9, 10, 1, 5, 3, 4, 0, 11, 8, 7, 2, 6]));
