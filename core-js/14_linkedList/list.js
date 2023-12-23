201224ed1c561f09166aa932b55634478e119b4d
asdacosta <asdacosta18@gmail.com>
2023-12-23 23:10:20 +0000

    Initialize classes

---
+++ core-js/14_linkedList/list.js
@@ -0,0 +1,14 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+class Node {
+  constructor(value, link) {
+    this.value = null;
+    this.link = null;
+  }
+}
+
+const firstLink = new LinkedList();
