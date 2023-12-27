4a43ccbdf75d42e1c0d1c18433d4a5bc390da86d
asdacosta <asdacosta18@gmail.com>
2023-12-27 07:51:40 +0000

    Add buildTree fxn and check index

--- core-js/15_binarySearch/bst.js
+++ core-js/15_binarySearch/bst.js
@@ -1,3 +1,22 @@
 function node(data, leftChild = null, rightChild = null) {
   return { data, leftChild, rightChild };
 }
+
+function buildTree(array) {
+  // Remove duplicate elements
+  const uniqueArray = array.filter((value, index, arr) => arr.indexOf(value) === index);
+  uniqueArray.sort((a, b) => a - b);
+
+  let rootIndex = Math.floor((uniqueArray.length - 1) / 2);
+  console.log(rootIndex);
+  let rootNode = uniqueArray[rootIndex];
+  console.log(rootNode);
+}
+
+function tree(array) {
+  root = buildTree(array);
+  return { root };
+}
+
+const shortArray = tree([1, 2, 3, 4]);
+shortArray.root;
