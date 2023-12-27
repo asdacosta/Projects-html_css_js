89ee0fc42cd6ca377bdb2ee59d455f535af89484
asdacosta <asdacosta18@gmail.com>
2023-12-27 14:23:11 +0000

    Add build tree factory function

--- core-js/15_binarySearch/bst.js
+++ core-js/15_binarySearch/bst.js
@@ -1,22 +1,30 @@
-function node(data, leftChild = null, rightChild = null) {
-  return { data, leftChild, rightChild };
+function node(data, left = null, right = null) {
+  return { data, left, right };
 }
 
 function buildTree(array) {
+  if (array.length === 0) {
+    return null;
+  }
+  if (array.length === 1) {
+    return node(array[0]);
+  }
+
   // Remove duplicate elements
   const uniqueArray = array.filter((value, index, arr) => arr.indexOf(value) === index);
   uniqueArray.sort((a, b) => a - b);
 
   let rootIndex = Math.floor((uniqueArray.length - 1) / 2);
-  console.log(rootIndex);
-  let rootNode = uniqueArray[rootIndex];
-  console.log(rootNode);
+  let rootValue = uniqueArray[rootIndex];
+  let leftSubTree = uniqueArray.slice(0, rootIndex);
+  let rightSubTree = uniqueArray.slice(rootIndex + 1);
+
+  let rootNode = node(rootValue, buildTree(leftSubTree), buildTree(rightSubTree));
+  return rootNode;
 }
 
 function tree(array) {
-  root = buildTree(array);
-  return { root };
+  rootNode = buildTree(array);
 }
 
-const shortArray = tree([1, 2, 3, 4]);
-shortArray.root;
+const shortArray = tree([3, 1, 3, 2, 4, 5, 5, 6, 7, 10, 8, 3, 9]);
