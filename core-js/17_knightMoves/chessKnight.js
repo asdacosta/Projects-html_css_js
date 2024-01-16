b7eaf3bcf0ecc4c712de9e09fb7a2e765ecb047b
asdacosta <asdacosta18@gmail.com>
2024-01-16 15:15:53 +0000

    Add move validator

--- core-js/17_knightMoves/chessKnight.js
+++ core-js/17_knightMoves/chessKnight.js
@@ -1,3 +1,10 @@
 const knightMoves = function (firstPoint, secPoint) {
   const moves = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)];
+
+  const validateMove = function (x, y) {
+    if (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+      return true;
+    }
+    return false;
+  };
 };
