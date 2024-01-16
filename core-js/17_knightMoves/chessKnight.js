6b96dbee2582a6349d7c3e549395135b1d369177
asdacosta <asdacosta18@gmail.com>
2024-01-16 21:36:10 +0000

    Search for shortest path with breadth search

--- core-js/17_knightMoves/chessKnight.js
+++ core-js/17_knightMoves/chessKnight.js
@@ -1,5 +1,14 @@
-const knightMoves = function (firstPoint, secPoint) {
-  const moves = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)];
+const knightMoves = function (initial, final) {
+  const moves = [
+    [-2, -1],
+    [-2, 1],
+    [-1, -2],
+    [-1, 2],
+    [1, -2],
+    [1, 2],
+    [2, -1],
+    [2, 1],
+  ];
 
   const validateMove = function (x, y) {
     if (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
@@ -20,4 +29,33 @@ const knightMoves = function (firstPoint, secPoint) {
     });
     return possibleMoves;
   };
+
+  const searchShortestPath = function (initial, final) {
+    const visited = new Set();
+    const queue = [[initial]];
+
+    while (queue.length > 0) {
+      const path = queue.shift();
+      const currentSquare = path[path.length - 1];
+
+      if (currentSquare[0] === final[0] && currentSquare[1] === final[1]) {
+        return path;
+      }
+
+      if (!visited.has(`${currentSquare[0]},${currentSquare[1]}`)) {
+        visited.add(`${currentSquare[0]},${currentSquare[1]}`);
+        const possibleMoves = getPossibleMoves(currentSquare);
+
+        for (const possibleMove of possibleMoves) {
+          if (!visited.has(`${possibleMove[0]},${possibleMove[1]}`)) {
+            queue.push([...path, possibleMove]);
+          }
+        }
+      }
+    }
+
+    return null;
+  };
 };
+
+// knightMoves([0, 0], [7, 7]);
