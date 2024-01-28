b98f6029d01a24afb0c98acbf0b51144d91dc7a2
asdacosta <asdacosta18@gmail.com>
2024-01-28 22:29:13 +0000

    Test reverseString function

--- core-js/18_jestTests/jest.test.js
+++ core-js/18_jestTests/jest.test.js
@@ -1,5 +1,10 @@
-const capitalize = require("./jest");
+const { capitalize, reverseString } = require("./jest");
 
 test("Capitalize first letter in string", () => {
   expect(capitalize("ever costa")).toBe("Ever costa");
+});
+
+test("Reverse string", () => {
+  const reversed = reverseString("ever");
+  expect(reversed).toBe("reve");
 });
