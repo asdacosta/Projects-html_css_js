9405ef1e431682d2e5e28d11f3e21782802704a2
asdacosta <asdacosta18@gmail.com>
2024-01-28 22:35:51 +0000

    Add calculator object

--- core-js/18_jestTests/jest.js
+++ core-js/18_jestTests/jest.js
@@ -6,7 +6,23 @@ const reverseString = function (string) {
   return string.split("").reverse().join("");
 };
 
+const calculator = {
+  add: function (m, n) {
+    return m + n;
+  },
+  subtract: function (m, n) {
+    return m - n;
+  },
+  divide: function (m, n) {
+    return m / n;
+  },
+  multiply: function (m, n) {
+    return m * n;
+  },
+};
+
 module.exports = {
   capitalize,
   reverseString,
+  calculator,
 };
